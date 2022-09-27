6840f41c6f667de1fede2347748990203623ce3e
Helder Freitas <freitas.h@gmail.com>
2022-09-27 15:34:10 -0300

    Aula 01 - Estrutura Base dos Projetos

---
+++ classroom/src/http/http.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver } from '@nestjs/apollo'
+
+import { DatabaseModule } from '../database/database.module';
+import { TestResolver } from './test.resolver';
+
+import path from 'node:path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [TestResolver],
+})
+
+export class HttpModule { }
+
